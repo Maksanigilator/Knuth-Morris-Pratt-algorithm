f6b74d4313e8ebae1abd55886652dd2d0421ecb5
Maksanigilator <161483269+Maksanigilator@users.noreply.github.com>
2024-06-12 18:08:13 +0300

    Add files via upload

---
+++ My.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace test_algoritmov
+{
+	public class My123
+	{
+        public static void NaiveSearch(string text, string pattern)
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Наивный поиск");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int n = text.Length;
+            int m = pattern.Length;
+            bool s = true;
+            int kolich = 0;
+            for (int i = 0; i <= n - m; i++)
+            {
+                //kolich++; ??????
+                s = true;
+                for (int j = 0; j < m; j++)
+                {
+                    kolich++;
+                    if (text[i + j] != pattern[j])
+                    {
+                        s = false;
+                        break;
+                    }
+                }
+
+                if (s == true)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            Console.WriteLine("сложность алгоритма = {0:d}, букв в слове = {1:d} ,букв в строке = {2:d}", kolich, n, m);
+            stopWatch.Stop();
+            TimeSpan ts1 = stopWatch.Elapsed;
+            Console.WriteLine(ts1);
+            if (s == false) Console.WriteLine("-1");
+
+            Console.WriteLine("----------------------------");
+        }
+
+
+        private static int[] GetPrefix(string s)
+        {
+            
+            int[] result = new int[s.Length];
+            result[0] = 0;
+            int index = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                int k = result[i - 1];
+                while (s[k] != s[i] && k > 0)
+                {
+                    k = result[k - 1];
+                }
+                if (s[k] == s[i])
+                {
+                    result[i] = k + 1;
+                }
+                else
+                {
+                    result[i] = 0;
+                }
+            }
+            
+            return result;
+        }
+
+        public static void FindSubstring(string text, string pattern)
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("КМП");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int kolich = 0;
+            int[] pf = GetPrefix(pattern);
+            int index = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                kolich++;
+                while (index > 0 && pattern[index] != text[i])
+                {
+                    index = pf[index - 1];
+                    kolich++;
+                }
+                if (pattern[index] == text[i]) index++;
+                if (index == pattern.Length)
+                {
+                    //return i - index + 1;
+                    Console.WriteLine(i - index + 1);
+                    break;
+                    
+                }
+            }
+            Console.WriteLine("сложность алгоритма = {0:d}, букв в слове = {1:d} ,букв в строке = {2:d}", kolich, text.Length, pattern.Length);
+
+            //return -1;
+            //Console.WriteLine("-1");
+            TimeSpan ts1 = stopWatch.Elapsed;
+            Console.WriteLine(ts1);
+
+        }
+
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Diagnostics;
+using System.Threading;
+using System.Text.RegularExpressions;
+class Program
+{
+
+    static void NaiveSearch(string text, string pattern)
+    {
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Наивный поиск");
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        int n = text.Length;
+        int m = pattern.Length;
+        bool s = true;
+        int kolich = 0;
+        for (int i = 0; i < n - m; i++)
+        {
+            kolich++;
+            s = true;
+            for (int j = 0; j < m; j++)
+            {
+                kolich++;
+                if (text[i + j] != pattern[j])
+                {
+                    s = false;
+                    break;
+                }
+            }
+
+            if (s == true)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+        }
+
+        Console.WriteLine("сложность алгоритма = {0:d}, букв в слове = {1:d} ,букв в строке = {2:d}", kolich, n, m);
+        stopWatch.Stop();
+        TimeSpan ts1 = stopWatch.Elapsed;
+        Console.WriteLine(ts1);
+        if (s == false) Console.WriteLine("-1");
+
+        Console.WriteLine("----------------------------");
+    }
+
+
+
+        static void test(out TimeSpan ts)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        Thread.Sleep(1000);
+        stopWatch.Stop();
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts1 = stopWatch.Elapsed;
+        //Console.WriteLine(ts1);
+        ts = ts1;
+    }
+
+
+
+    static void Main()
+    {
+        string a0 = "abbbbbbbbbbba";
+        string b = "abbbbbbbbbbba";
+        string b0 = "ba";
+        string a1 = "abcdabdadddeeerd";
+        string f = "gfmddeuddfddeuddspddeuddgddebubcddeuddrythpddeuddlustddeuddqderbeyczkmgfmddeudgfgfmddeuddfspggfmddeuddfspgddebubcddeuddrythpddeuddlustddeuddqderbeyczkmgfmddeudgfgfmddeuddfspgddebubcrythplustqderbeyczkmgfmddeudgfmddeuddfspgddebubcrythplustqdmddeuddfspgddebubcrythplustqderbeyczkmgfmddeuddeerbeyrы";
+        string d = "г";
+        //string d = "ddeudde";
+        TimeSpan t;
+        //test(out t);
+        //NaiveSearch(a, b);
+        //My123.NaiveSearch(a1, b);
+        //test_algoritmov.My123.NaiveSearch(a1, b);
+        //test_algoritmov.My123.FindSubstring(a1, b);
+        //Console.WriteLine(j);
+        //test_algoritmov.My1234.bm(a1, b);
+        test_algoritmov.My123.FindSubstring(a0, b);
+        test_algoritmov.My123.NaiveSearch(a0, b);
+    }
+}
